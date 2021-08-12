ff521c43637087aa82e6e84f6615b5566a5977f2
ass <holen@bitcoin.com>
2021-08-12 19:04:50 +1000

    added stolbik. fixed mode choose

--- cred_maap.py
+++ cred_maap.py
@@ -2,7 +2,7 @@ HOST_URL ='http://192.168.1.200'
 SENDER_MAIL = 'maap.acct.kuz@gmail.com'
 
 GMAIL_ACCT = 'maap.acct.kuz@gmail.com'
-GMAIL_PASSWD = 'thspider254770!A'
+GMAIL_PASSWD = ''
 
 #echo 'export PYTHONPATH="${PYTHONPATH}:/home/user/django/maap/mainapp"' >> venv/bin/activate
 #echo 'export PYTHONPATH="${PYTHONPATH}:/home/user/django/"' >> venv/bin/activate
