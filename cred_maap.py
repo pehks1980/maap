2b84b89a2b1da9fccacd0370d2ff677aabca90a8
ass <holen@bitcoin.com>
2021-08-12 19:03:26 +1000

    added stolbik. fixed mode choose

---
+++ cred_maap.py
@@ -0,0 +1,12 @@
+HOST_URL ='http://192.168.1.200'
+SENDER_MAIL = 'maap.acct.kuz@gmail.com'
+
+GMAIL_ACCT = 'maap.acct.kuz@gmail.com'
+GMAIL_PASSWD = 'thspider254770!A'
+
+#echo 'export PYTHONPATH="${PYTHONPATH}:/home/user/django/maap/mainapp"' >> venv/bin/activate
+#echo 'export PYTHONPATH="${PYTHONPATH}:/home/user/django/"' >> venv/bin/activate
+#echo 'echo PYTHONPATH = $PYTHONPATH' >> venv/bin/activate
+#source  venv/bin/activate
+
+# install pip django_cron 
