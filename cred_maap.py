b9fc052d99dd4734dbfc59045b81f4061c46713d
ass <holen@bitcoin.com>
2022-07-15 20:54:24 +1000

    removedstatic1

--- cred_maap.py
+++ cred_maap.py
@@ -4,7 +4,7 @@ SENDER_MAIL = 'maap.acct.kuz@gmail.com'
 GMAIL_ACCT = 'maap.acct.kuz@gmail.com'
 GMAIL_PASSWD = ''
 
-#echo 'export PYTHONPATH="${PYTHONPATH}:/home/user/django/maap/mainapp"' >> venv/bin/activate
+# echo 'export PYTHONPATH="${PYTHONPATH}:/home/user/django/maap/mainapp"' >> venv/bin/activate
 #echo 'export PYTHONPATH="${PYTHONPATH}:/home/user/django/"' >> venv/bin/activate
 #echo 'echo PYTHONPATH = $PYTHONPATH' >> venv/bin/activate
 #source  venv/bin/activate
