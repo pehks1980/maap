19fc687b41ff247d4b68b1187e8bce19d5e22a80
ass <holen@bitcoin.com>
2022-07-18 14:46:42 +1000

    removedstatic1-1

--- cred_maap.py
+++ cred_maap.py
@@ -5,8 +5,8 @@ GMAIL_ACCT = 'maap.acct.kuz@gmail.com'
 GMAIL_PASSWD = ''
 
 # echo 'export PYTHONPATH="${PYTHONPATH}:/home/user/django/maap/mainapp"' >> venv/bin/activate
-#echo 'export PYTHONPATH="${PYTHONPATH}:/home/user/django/"' >> venv/bin/activate
-#echo 'echo PYTHONPATH = $PYTHONPATH' >> venv/bin/activate
-#source  venv/bin/activate
+# echo 'export PYTHONPATH="${PYTHONPATH}:/home/user/django/"' >> venv/bin/activate
+# echo 'echo PYTHONPATH = $PYTHONPATH' >> venv/bin/activate
+# source  venv/bin/activate
 
 # install pip django_cron 
